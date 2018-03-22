2094ad60d29ecdab096469ccf7ffae89dff2cf43
Sam Luby <samwluby@gmail.com>
2018-03-22 18:02:42 +0000

    Seperated panthomp into functions

--- pan-thomp.py
+++ pan-thomp.py
@@ -12,15 +12,18 @@ import warnings
 # ignore pandas warning about deprecated function
 warnings.simplefilter(action='ignore', category=FutureWarning)
 results = {}
+file_name = 'sample-data/ecgdata2min.csv'
 
 
-file = 'sample-data/skx-data-10ksamp.txt'
-with open(file) as file_object:
-    #next(file_object)
-    lines = file_object.readlines()
-for i, line in enumerate(lines):
-    lines[i] = float(line.strip())
-    line = float(line.strip())
+# open file
+def open_data_file(file):
+    with open(file) as file_object:
+        # next(file_object)
+        lines = file_object.readlines()
+    for i, line in enumerate(lines):
+        lines[i] = float(line.strip())
+    return lines
+
 
 # scaling preprocess
 def data_scaling(dataset):
@@ -30,67 +33,58 @@ def data_scaling(dataset):
     return dataset
 
 
-data = data_scaling(lines)
-data = data[2000:4000].reset_index()
+def filter_signal(noisy_signal, high, low, fs):
+    fs = 250
+    nyq = 0.5 * fs
+    fc_high = high
+    fc_low = low
+    B, A = butter(2, fc_low / nyq, btype='low') #2nd order BW LowPassFilter
+    filtered_signal = lfilter(B, A, noisy_signal, axis=0)
+    B, A = butter(2, fc_high / nyq, btype='high')  # 2nd order BW HighPassFilter
+    filtered_signal = lfilter(B, A, filtered_signal, axis=0)
+    # some amplification
+    return filtered_signal
 
-## Add some noise
-data['noise'] = data['ecgdat'] + np.random.normal(0, 0.75, len(data))
+
+def get_time_period(fs):
+    T = (1/fs)*len(lines)
+    n = len(data['filtered'])
+    print("Length n samples:", n)
+    t = np.linspace(0, T, n, endpoint=False)
+    return n, t
 
 
-############################
-#     Signal Filtering     #
-############################
-fs = 150
-nyq = 0.5 * fs
-# T = 40.0         # seconds
-T = (1/fs)*len(lines)
-fc_high = 15
-fc_low = 5
-B, A = butter(2, fc_low / nyq, btype='low') #2nd order BW LowPassFilter
-filtered_signal = lfilter(B, A, data['noise'], axis=0)
-B, A = butter(2, fc_high / nyq,  btype='high') #2nd order BW HighPassFilter
-filtered_signal = lfilter(B, A, filtered_signal, axis=0)
-
-# some amplification
-data['filtered'] = filtered_signal*10
-
-n = len(filtered_signal)
-print("Length n samples:", n)
-t = np.linspace(0, T, n, endpoint=False)
-
+#  TODO Change this (input signal is not really noisy)
 # Plot input signal & filtered signal
-plt.subplot(2, 1, 1)
-plt.plot(t[1:n], data['noise'][1:n], 'b-', linewidth = 0.5, label='noisy')
-plt.plot(t[1:n], data['filtered'][1:n], 'r-', linewidth=1, label='filtered data')
-plt.xlabel('Time [sec]')
-plt.grid()
-plt.legend()
-plt.subplots_adjust(hspace=0.35)
+def plot_input_and_filtered(input, filtered, n ,t):
+    # input = data['noise']
+    # filtered = data['filtrered']
+    plt.subplot(2, 1, 1)
+    plt.plot(t[1:n], input[1:n], 'b-', linewidth = 0.5, label='noisy')
+    plt.plot(t[1:n], filtered[1:n], 'r-', linewidth=1, label='filtered data')
+    plt.xlabel('Time [sec]')
+    plt.grid()
+    plt.legend()
+    plt.subplots_adjust(hspace=0.35)
 
 
-############################
-#  Derivation % Squaring   #
-############################
-h = [-1., -2., 0., 2., 1.]
-derivated_signal = signal.convolve(filtered_signal, h)
-derivated_signal = derivated_signal**2
-data['derivated'] = derivated_signal[:n]
+#  derivate and square signal
+def derivate_signal(data):
+    h = [-1., -2., 0., 2., 1.]
+    derivated_signal = signal.convolve(data, h)
+    derivated_signal = derivated_signal ** 2
+    deriv = derivated_signal[:n]
+    return deriv
 
 
-################################
-## Moving Window/Rolling Mean ##
-################################
+# rolling mean/moving average
 def moving_average(data, hrw, fs):
-    mov_avg = pd.Series(data.rolling(window=int(hrw * fs)).mean()) # pandas new rolling mean method, but slower than deprecated method
-    # mov_avg = pd.rolling_mean(dataset, window=int(hrw * fs))
+    # mov_avg = pd.Series(data.rolling(window=int(hrw * fs)).mean()) # pandas new rolling mean method, but slower than deprecated method
+    mov_avg = pd.rolling_mean(data, window=int(hrw * fs))
     avg_heart_rate = np.mean(data)
     mov_avg = [avg_heart_rate if math.isnan(x) else x for x in mov_avg]
-    # print(mov_avg)
-    return [1000 + x * 2.5 for x in mov_avg]
-
-
-data['avg'] = moving_average(data['derivated'], 0.125, fs)
-# print(data[0:10])
+    avg = [1000 + x * 2.5 for x in mov_avg]
+    return avg
 
 
 # detects and locates the x location (time) of the ECG's R peaks
@@ -100,21 +94,15 @@ def detect_R_peaks(data):
     count = 0
     avg = data['avg'].tolist()
     deriv = data['derivated'].tolist()
-    dat = data['ecgdat'].tolist()
-    # print(avg[0:10])
 
-    # TODO Some calculation errors in here [count seems to be wrong]
     for ecg_val in deriv:
         rollingmean = int(avg[count])
-        # print("Roll-mean:", rollingmean, dat[count])
         if (int(ecg_val) <= rollingmean) and (len(window) <= 1): # Here is the update in (ecg_val <= rollingmean)
             count += 1
         elif int(ecg_val) > rollingmean:
             window.append(ecg_val)
             count += 1
         else:
-            # print("WIndow length:", len(window), "Window index max:", window.index(max(window)))
-            # print(count)
             beatposition = count - len(window) + (window.index(max(window)))
             R_peak_locations.append(beatposition)
             window = []
@@ -147,6 +135,26 @@ def calculate_bpm():
 
 
 
+##############
+#    Main    #
+##############
+lines = open_data_file(file_name)
+data = data_scaling(lines)
+data = data[0:2500].reset_index()
+fs = 250
+
+## Add some noise
+data['noise'] = data['ecgdat'] + np.random.normal(0, 0.75, len(data))
+data['filtered'] = filter_signal(data['noise'], 15, 5, 250)
+n, t = get_time_period(fs)
+
+plot_input_and_filtered(data['noise'], data['filtered'], n, t)
+
+data['derivated'] = derivate_signal(data['filtered'])
+data['avg'] = moving_average(data['derivated'], 0.125, fs)
+
+print(data[100:200])
+
 detect_R_peaks(data)
 calculate_RR_intervals(fs)
 calculate_bpm()
@@ -154,7 +162,7 @@ calculate_bpm()
 plt.subplot(2, 1, 2)
 # plt.plot(t[1:n], data['filtered'][1:n], 'b-', label='data')
 plt.plot(data['derivated'], 'b-', linewidth=1)
-plt.plot( data['avg'], 'g-')
+plt.plot(data['avg'], 'g-')
 plt.scatter(results['R_peak_X_locations'], results['R_peak_Y_locations'], color='red', label="Average heart rate: %.2f BPM" % results['bpm'])
 plt.xlabel('Time [sec]')
 plt.grid()
