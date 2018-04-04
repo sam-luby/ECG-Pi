836c3dd9fadb417a0ba25e9263756b6f5419589e
Sam Luby <samwluby@gmail.com>
2018-04-04 23:23:02 +0000

    Fix filename structure

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ fs = 250
 fc_low = 5
 fc_high = 35
 now = datetime.datetime.now()
-file_suffix= now.strftime("%d-%m-%Y %Hh%Mm.csv")
+file_suffix= now.strftime("%d-%m-%Y_%Hh%Mm.csv")
 Nsamp = T*fs
 
 ##############
 #    Main    #
 ##############
 if mode == 1:
-    filename = ("sample-data/" + "Arduino " + str(T) + "secs " + file_suffix)
+    filename = ("sample-data/" + "Arduino_" + str(T) + "secs_" + file_suffix)
     Nsamp = ard.get_data_from_arduino(T, filename)
     pt.run_pan_thomp(filename, fs, fc_high, fc_low, Nsamp)  # Run Pan Thompkins algorithm on collected ECG data
     #os.remove(filename)                                     # Remove file after processing
 elif mode == 2:
-    #filename = ("sample-data/" + "RPI " + str(T) + "secs " + file_suffix)
-    filename = "test_pigpio_out.csv"
+    filename = ("sample-data/" + "RPI_" + str(T) + "secs_" + file_suffix)
+    #filename = "test_pigpio_out.csv"
     commandline = "sudo ./readmcp3008 " + filename
     print(commandline) 
     os.system(commandline)
