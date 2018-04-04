34d8e0446dff57188bb0c9db8433feb638a8ed60
Sam Luby <samwluby@gmail.com>
2018-04-04 16:11:09 +0100

    Changes to MCP

--- from_MCP3008.py
+++ from_MCP3008.py
@@ -9,16 +9,23 @@ def get_data_from_MCP(T):
     fs = 250
     i = 0
     Nsamp = T*fs
+    milestones = []
     SPI_PORT = 0
     SPI_DEVICE = 0
+    for x in range(1, 11):  # List to store Nsamp/multiples of 10 for %age calc
+        milestones.append(int(x * (Nsamp / 10)))
     mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
     values = []
+    percentage = 10
 
     print('Reading MCP3008 values, press Ctrl-C to quit...')
     while True and i < Nsamp:
         value = mcp.read_adc(0)
         values.append(value)
         i+=1
+        if i in milestones:  # Silly %age completed indication for user
+            print("Collecting data, {}% complete.".format(percentage))
+            percentage += 10
         time.sleep(0.004)
         
     dat = pd.DataFrame(values)
